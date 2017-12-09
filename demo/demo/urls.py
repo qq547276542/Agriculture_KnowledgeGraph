4320703c9668b64265262d3a46747cb70f19775c
qq547276542 <547276542@qq.com>
2017-12-09 11:49:36 +0800

    制作了训练集标注页面

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url 
 from . import index_view,index_ERform_view,detail_view
- 
+from . import  tagging_data_view,tagging_data_writefile_view
+
 urlpatterns = [
     url(r'^$', index_view.index),
     url(r'^ER-post',index_ERform_view.ER_post),
     url(r'^detail', detail_view.showdetail),
+    url(r'^tagging_data', tagging_data_view.showtagging_data),
+    url(r'^tagging-get', tagging_data_writefile_view.tagging_push),
 ]
