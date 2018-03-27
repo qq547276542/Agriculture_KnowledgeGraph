f1853a63f302c31ee5228ab20dbf674aafe37813
qq547276542 <547276542@qq.com>
2018-03-27 08:54:15 +0800

    delete my password

--- demo/Model/neo_models.py
+++ demo/Model/neo_models.py
@@ -4,10 +4,10 @@ class Neo4j():
 	graph = None
 	def __init__(self):
 		print("create neo4j class ...")
-		
+
 	def connectDB(self):
-		self.graph = Graph("http://localhost:7474", username="neo4j", password="8313178")
-		
+		self.graph = Graph("http://localhost:7474", username="neo4j", password="123456")
+
 	def matchItembyTitle(self,value):
 		answer = self.graph.find_one(label="Item",property_key="title",property_value=value)
 		return answer
